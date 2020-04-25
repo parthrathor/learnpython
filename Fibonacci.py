8f044957ab2106558661ae44f5fda4adea351a1a
parthrathor <parth.rathor.10@gmail.com>
2020-04-25 20:12:21 +0530

    2nd commit

--- Fibonacci.py
+++ Fibonacci.py
@@ -2,9 +2,24 @@
 # Have the user enter a number and
 # generate a fibonacci sequence
 # which size is equivalent to that number.
-
-def finacciseq(n)
+def fibonacciseq(n):
     assert n>0
     series = [1]
 
     while len(series) < n:
+        if len(series) == 1:
+            series.append(1)
+        else:
+            series.append(series[-1] + series[-2])
+
+    for i in range(len(series)):  # Convert the numbers to strings
+            series[i] = str(series[i])
+
+    return (','.join(series))  # Return the sequence separated by commas
+
+def main():  # Wrapper function
+
+    print(fibonacciseq(int(input('How many numbers do you need? '))))
+
+if __name__ == '__main__':
+    main()
